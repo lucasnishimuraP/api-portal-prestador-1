0b4040fcdc66881fdc6ab1f7a598c453cf2b08b0
TI Corporativa <ticorp@LucasNishimura.local>
2020-01-07 14:38:16 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var mysqlConnection = mysql.createConnection({
+
+    host : "localhost",
+    user : "root",
+    password : "password",
+    database : "Edureka",
+    multipleStatements : true
+})
+
+mysqlConnection.connect((err)=>{
+
+    if(!err) {
+            console.log("Connected !! LNP")
+     }else{
+         console.log("Connection Failed !")
+     }
+
+})
+
+app.listen(3000);
